2e768956a66b7e0eb1e963a541a7ac3c2e4a4180
ambitionlion <123803602+ambitionlion@users.noreply.github.com>
2023-07-26 15:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const API_KEY = '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';
+const submitButton = document.querySelector('#submit');
+const outPutElement = document.querySelector('#output');
+const inputElement = document.querySelector('input');
+const historyElement = document.querySelector('.history');
+const buttonElement = document.querySelector('button');
+
+function changeInput(value)
+{
+   const inputElement=document.querySelector('input')
+   inputElement.value=value;
+}
+async function getMessage() {
+  console.log('clicked');
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: inputElement.value}],
+      max_tokens: 100
+    })
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    console.log(data);
+    outPutElement.textContent = data.choices[0].message.content;
+
+    if (data.choices[0].message.content) {
+      const pElement = document.createElement('p');
+      pElement.textContent = inputElement.value;
+      historyElement.append(pElement);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+submitButton.addEventListener('click', getMessage);
